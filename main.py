6927cbcf45f95f7d1708399f48ea5b91abc10878
Nawres <nawresjouini@gmail.com>
2023-08-07 12:39:31 +0100

    Modified the main

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+class Preprocessor:
+    def __init__(self):
 
+
+    def preprocess_cv(self):
+
+
+    def preprocess_job(self):
+
+
+class Embedder:
+    def __init__(self):
+
+
+    def get_embeddings(self):
+
+
+class similarityCalculator :
+    def __init__(self):
+
+
+    def calculate_similarity(self):
